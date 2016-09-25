1d57ea8697c3bbf192c848b1169aa51f54f6035f
Robert Rabe <robert.rabe1@icloud.com>
2016-09-25 18:13:42 +0200

    Configure the job list to take in views

--- JenkinsiOS/JobList.swift
+++ JenkinsiOS/JobList.swift
@@ -11,7 +11,9 @@ import Foundation
 class JobList: CustomStringConvertible{
     
     var jobs: [Job] = []
+    var views: [View]?
     
+    //FIXME: Init with different tree
     init(data: Any) throws{
         guard let json = data as? [String: AnyObject]
             else { throw ParsingError.DataNotCorrectFormatError }
@@ -23,11 +25,22 @@ class JobList: CustomStringConvertible{
                 jobs.append(job)
             }
         }
+        
+        if let jsonViews = json["views"] as? [[String: AnyObject]]{
+            views = []
+            for jsonView in jsonViews{
+                if let view = View(json: jsonView){
+                    views?.append(view)
+                }
+            }
+        }
     }
     
     var description: String{
         return "{\n" + jobs.reduce("", { (result, job) -> String in
             return "\(result) Name: \(job.name), Description: \(job.description) \n"
-        }) + "}"
+        }) + (views?.reduce("Views: ", { (result, view) -> String in
+            return "\(result)\(view)\n"
+        }) ?? "No views") + "}"
     }
 }
