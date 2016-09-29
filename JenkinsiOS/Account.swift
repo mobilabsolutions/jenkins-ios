a37554dbe53c1570b2dd91f8649f05aee5a77e0d
Robert Rabe <robert.rabe1@icloud.com>
2016-09-29 19:22:12 +0200

    Let account be NSCoding compatible

--- JenkinsiOS/Account.swift
+++ JenkinsiOS/Account.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Account{
+class Account: NSObject, NSCoding{
     var baseUrl: URL
     var username: String?
     var password: String?
@@ -19,5 +19,20 @@ class Account{
         self.username = username
         self.password = password
         self.port = port
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        guard let baseUrl = aDecoder.decodeObject(forKey: "baseUrl") as? URL
+            else { return nil }
+        
+        self.baseUrl = baseUrl
+        port = aDecoder.decodeObject(forKey: "port") as? Int
+        username = aDecoder.decodeObject(forKey: "username") as? String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(baseUrl, forKey: "baseUrl")
+        aCoder.encode(port, forKey: "port")
+        aCoder.encode(username, forKey: "username")
     }
 }
