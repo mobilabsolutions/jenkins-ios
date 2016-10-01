925607926636844879c9ac01928420b28ad7c20c
Robert Rabe <robert.rabe1@icloud.com>
2016-10-01 19:12:23 +0200

    Views should have jobs belonging to them

--- JenkinsiOS/View.swift
+++ JenkinsiOS/View.swift
@@ -11,16 +11,26 @@ import Foundation
 class View: CustomStringConvertible{
     var name: String
     var url: URL
+    var jobs: [Job] = []
     
     var description: String{
         return "View \"\(name)\""
     }
     
     init?(json: [String: AnyObject]){
-        guard let name = json["name"] as? String, let urlString = json["url"] as? String,
+        guard let name = json[Constants.JSON.name] as? String, let urlString = json[Constants.JSON.url] as? String,
             let url = URL(string: urlString)
             else { return nil }
         self.name = name
         self.url = url
+        
+        if let jobsJson = json[Constants.JSON.jobs] as? [[String: AnyObject]]{
+            for jobJson in jobsJson{
+                if let job = Job(json: jobJson, minimalVersion: true){
+                    jobs.append(job)
+                }
+            }
+        }
+        
     }
 }
