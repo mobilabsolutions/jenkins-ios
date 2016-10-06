bac32ac397f002b31d1713186e6350d8672e240a
Robert Rabe <robert.rabe1@icloud.com>
2016-10-06 16:54:00 +0200

    Add picker to JobsTableViewController

--- JenkinsiOS/JobsTableViewController.swift
+++ JenkinsiOS/JobsTableViewController.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class JobsTableViewController: UITableViewController{
+    
     var account: Account?
     var jobs: JobList?
+    var currentView: View?
+    
+    @IBOutlet weak var viewPicker: UIPickerView!
     
     override func viewDidLoad() {
         loadJobs()
+        setUpPicker()
     }
     
+    /// Load the jobs from the remote server
     func loadJobs(){
         guard let account = account
             else { return }
@@ -23,8 +29,11 @@ class JobsTableViewController: UITableViewController{
             //FIXME: Display an error message on error
             if error == nil && jobList != nil{
                 self.jobs = jobList
+                self.currentView = jobList!.allJobsView
                 
                 DispatchQueue.main.async {
+                    self.viewPicker.reloadAllComponents()
+                    self.pickerScrollToAllView()
                     self.tableView.reloadData()
                 }
                 
@@ -33,13 +42,24 @@ class JobsTableViewController: UITableViewController{
                 print("Error: \(error)")
             }
         }
-
+    }
+    
+    func setUpPicker(){
+        viewPicker.dataSource = self
+        viewPicker.delegate = self
+    }
+    
+    
+    private func pickerScrollToAllView(){
+        if let jobs = jobs, let currentView = currentView, let index = jobs.views.index(where: {$0.name == currentView.name}){
+            viewPicker.selectRow(index, inComponent: 0, animated: true)
+        }
     }
     
     //MARK: - Viewcontroller navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let dest = segue.destination as? JobViewController, segue.identifier == Constants.Identifiers.showJobSegue, let jobCell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: jobCell), let job = jobs?.allJobsView?.jobs[indexPath.row]{
+        if let dest = segue.destination as? JobViewController, segue.identifier == Constants.Identifiers.showJobSegue, let jobCell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: jobCell), let job = currentView?.jobs[indexPath.row]{
             dest.job = job
             dest.account = account
         }
@@ -49,12 +69,33 @@ class JobsTableViewController: UITableViewController{
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.Identifiers.jobCell, for: indexPath)
-        cell.textLabel?.text = jobs?.allJobsView?.jobs[indexPath.row].name
-        cell.detailTextLabel?.text = jobs?.allJobsView?.jobs[indexPath.row].color?.rawValue ?? jobs?.allJobsView?.jobs[indexPath.row].description
+        
+        cell.textLabel?.text = currentView?.jobs[indexPath.row].name
+        cell.detailTextLabel?.text = currentView?.jobs[indexPath.row].color?.rawValue ?? currentView?.jobs[indexPath.row].description
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return jobs?.allJobsView?.jobs.count ?? 0
+        return currentView?.jobs.count ?? 0
+    }
+}
+
+extension JobsTableViewController: UIPickerViewDataSource, UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return jobs?.views.count ?? 0
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return jobs?.views[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currentView = jobs?.views[row]
+        tableView.reloadData()
     }
 }
