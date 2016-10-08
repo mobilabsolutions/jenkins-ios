043f2f45671503312f1387eb2f1d54b9f42d2c03
Robert Rabe <robert.rabe1@icloud.com>
2016-10-08 13:54:36 +0200

    Add convenience methods for getting special user requests

--- JenkinsiOS/UserRequest.swift
+++ JenkinsiOS/UserRequest.swift
@@ -34,3 +34,76 @@ class UserRequest{
         self.additionalQueryItems = additionalQueryItems
     }
 }
+
+//MARK: - Convenience functions for special user requests
+extension UserRequest{
+    
+    /// Return the user request specific to getting the list of plugins
+    ///
+    /// - parameter account: The account for which the user request should be create
+    ///
+    /// - returns: The fitting user request object
+    static func userRequestForPlugins(account: Account) -> UserRequest{
+        let url = account.baseUrl.appendingPathComponent(Constants.API.plugins)
+        let additionalComponents = Constants.API.pluginsAdditionalQueryItems
+        
+        return UserRequest(requestUrl: url, account: account, additionalQueryItems: additionalComponents)
+    }
+    
+    /// Return the user request specific to getting the list of users
+    ///
+    /// - parameter account: The account for which the user request should be create
+    ///
+    /// - returns: The fitting user request object
+    static func userRequestForUsers(account: Account) -> UserRequest{
+        let url = account.baseUrl.appendingPathComponent(Constants.API.users)
+        
+        return UserRequest(requestUrl: url, account: account, additionalQueryItems: additionalComponents)
+    }
+    
+    /// Return the user request specific to getting the list of jobs
+    ///
+    /// - parameter account: The account for which the user request should be create
+    ///
+    /// - returns: The fitting user request object
+    static func userRequestForJobList(account: Account) -> UserRequest{
+        let url = account.baseUrl
+        let additionalComponents = Constants.API.jobListAdditionalQueryItems
+        
+        return UserRequest(requestUrl: url, account: account, additionalQueryItems: additionalComponents)
+    }
+    
+    /// Return the user request specific to getting the list of test reports
+    ///
+    /// - parameter account: The account for which the user request should be create
+    ///
+    /// - returns: The fitting user request object
+    static func userRequestForTestReport(account: Account) -> UserRequest{
+        let url = account.baseUrl.appendingPathComponent(Constants.API.testReport)
+        let additionalComponents = Constants.API.testReportAdditionalQueryItems
+        
+        return UserRequest(requestUrl: url, account: account, additionalQueryItems: additionalComponents)
+    }
+    
+    /// Return the user request specific to getting the list of computers
+    ///
+    /// - parameter account: The account for which the user request should be create
+    ///
+    /// - returns: The fitting user request object
+    static func userRequestForComputers(account: Account) -> UserRequest{
+        let url = account.baseUrl.appendingPathComponent(Constants.API.computer)
+        
+        return UserRequest(requestUrl: url, account: account, additionalQueryItems: additionalComponents)
+    }
+    
+    /// Return the user request specific to getting the build queue
+    ///
+    /// - parameter account: The account for which the user request should be create
+    ///
+    /// - returns: The fitting user request object
+    static func userRequestForBuildQueue(account: Account) -> UserRequest{
+        let url = account.baseUrl.appendingPathComponent(Constants.API.buildQueue)
+        
+        return UserRequest(requestUrl: url, account: account, additionalQueryItems: additionalComponents)
+    }
+}
