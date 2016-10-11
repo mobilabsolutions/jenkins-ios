ff55247715fe1ded27d64f9434ed6b4662c01e49
Robert Rabe <robert.rabe1@icloud.com>
2016-10-11 10:03:26 +0200

    Rewrite Favorite to take in an account as parameter. Refactor Favoritable; use method toggleFavorite instead of setting isFavorite

--- JenkinsiOS/Favoratible.swift
+++ JenkinsiOS/Favoratible.swift
@@ -10,7 +10,8 @@ import Foundation
 
 protocol Favoratible {
     var url: URL{get}
-    var isFavorite: Bool {get set}
+    var isFavorite: Bool {get}
+    func toggleFavorite(account: Account)
 }
 
 extension Favoratible{
@@ -20,9 +21,9 @@ extension Favoratible{
         
         switch self{
         case is Job:
-            type = .Job
+            type = .job
         case is Build:
-            type = .Build
+            type = .build
         default:
             type = nil
         }
@@ -30,21 +31,22 @@ extension Favoratible{
         return type
     }
     
+    func toggleFavorite(account: Account){
+        if let type = getType(){
+            if let index = ApplicationUserManager.manager.applicationUser.favorites.index(of: Favorite(url: self.url, type: type, account: account)){
+                ApplicationUserManager.manager.applicationUser.favorites.remove(at: index)
+            }
+            else{
+                ApplicationUserManager.manager.applicationUser.favorites.append(Favorite(url: self.url, type: type, account: account))
+            }
+        }
+    }
+    
     var isFavorite: Bool{
         get{
             guard let type = getType()
                 else { return false }
-            return ApplicationUserManager.manager.applicationUser.favorites.contains(Favorite(url: self.url, type: type))
-        }
-        set{
-            if let type = getType(){
-                if !newValue, let index = ApplicationUserManager.manager.applicationUser.favorites.index(of: Favorite(url: self.url, type: type)){
-                    ApplicationUserManager.manager.applicationUser.favorites.remove(at: index)
-                }
-                else{
-                    ApplicationUserManager.manager.applicationUser.favorites.append(Favorite(url: self.url, type: type))
-                }
-            }
+            return ApplicationUserManager.manager.applicationUser.favorites.contains(Favorite(url: self.url, type: type, account: nil))
         }
     }
 }
--- JenkinsiOS/Favorite.swift
+++ JenkinsiOS/Favorite.swift
@@ -11,28 +11,34 @@ import Foundation
 class Favorite: NSObject, NSCoding{
     
     enum FavoriteType: String{
-        case Job = "Job"
-        case Build = "Build"
+        case job = "Job"
+        case build = "Build"
     }
     
     var type: FavoriteType
     var url: URL
+    var account: Account?
     
-    init(url: URL, type: FavoriteType){
+    init(url: URL, type: FavoriteType, account: Account?){
         self.type = type
         self.url = url
+        self.account = account
     }
     
     required init?(coder aDecoder: NSCoder) {
-        guard let url = aDecoder.decodeObject(forKey: "url") as? URL, let type = aDecoder.decodeObject(forKey: "type") as? String
+        guard let url = aDecoder.decodeObject(forKey: "url") as? URL, let type = aDecoder.decodeObject(forKey: "type") as? String, let accountUrl = aDecoder.decodeObject(forKey: "accountUrl") as? URL
             else { return nil }
         self.url = url
         self.type = FavoriteType(rawValue: type)!
+        guard let account = AccountManager.manager.accounts.first(where: {$0.baseUrl == accountUrl})
+            else { return nil }
+        self.account = account
     }
     
     func encode(with aCoder: NSCoder) {
         aCoder.encode(url, forKey: "url")
         aCoder.encode(type.rawValue, forKey: "type")
+        aCoder.encode(account?.baseUrl, forKey: "accountUrl")
     }
     
     override func isEqual(_ object: Any?) -> Bool {
