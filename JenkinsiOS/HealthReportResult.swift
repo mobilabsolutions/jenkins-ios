9047499d99e04386aa1056a69985b21fa4a34245
Robert Rabe <robert.rabe1@icloud.com>
2016-10-11 10:52:27 +0200

    Remove unneeded icon url in HealthReportResult

--- JenkinsiOS/HealthReportResult.swift
+++ JenkinsiOS/HealthReportResult.swift
@@ -11,23 +11,17 @@ import Foundation
 class HealthReportResult{
     
     var description: String
-    //FIXME: This should be the actual url of the item, not only a part of its path
-    var iconUrl: String
     var score: Int
     var iconClassName: String
     
     init?(json: [String: AnyObject]){
         guard let description = json["description"] as? String,
-            let iconUrl = json["iconUrl"] as? String,
             let score = json["score"] as? Int,
             let iconClassName = json["iconClassName"] as? String
             else{ return nil }
         
         self.description = description
-        self.iconUrl = iconUrl
         self.score = score
         self.iconClassName = iconClassName
-        
-        print("Icon url: \(iconUrl)")
     }
 }
