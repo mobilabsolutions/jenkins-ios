801250a50b43d08b920109d21bb714ae1da1fb8c
Robert Rabe <robert.rabe1@icloud.com>
2016-10-13 09:47:04 +0200

    Add extensions for printing out TimeIntervals, as well as showing error messages. Set textify nil text to "Unknown"

--- JenkinsiOS/Extensions.swift
+++ JenkinsiOS/Extensions.swift
@@ -31,9 +31,56 @@ extension Optional{
     func textify() -> String{
         switch self{
             case .none:
-                return "nil"
+                return "Unknown"
             default:
                 return "\(self!)"
         }
+    }
+}
+
+extension TimeInterval{
+    
+    /// Convert a TimeInterval to a string describing it
+    ///
+    /// - returns: A string describing the TimeInterval in the form: xx hours
+    ///            yy minutes zz seconds
+    func toString() -> String{
+        let seconds = (self / 1000).truncatingRemainder(dividingBy: 60)
+        let minutes = (self / 60000).truncatingRemainder(dividingBy: 60)
+        let hours = self / 3600000
+
+        var returnString = ""
+        
+        if Int(hours) > 0{
+            returnString += "\(Int(hours)) hours "
+        }
+        if Int(minutes) > 0{
+            returnString += "\(Int(minutes)) minutes "
+        }
+        if Int(seconds) > 0{
+            returnString += "\(Int(seconds)) seconds"
+        }
+        
+        return returnString
+    }
+}
+
+extension UIViewController{
+    /// Display an error with given title, message, textfields and alert actions
+    ///
+    /// - parameter title:      The title of the error
+    /// - parameter message:    The error message
+    /// - parameter textFields: The text fields that should be displayed
+    /// - parameter actions:    The actions that should be displayed
+    func displayError(title: String, message: String?, textFieldConfigurations: [(UITextField) -> ()], actions: [UIAlertAction]){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        actions.forEach { (action) in
+            alertController.addAction(action)
+        }
+        textFieldConfigurations.forEach { (textFieldConfiguration) in
+            alertController.addTextField(configurationHandler: textFieldConfiguration)
+        }
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
