a4635d7c831e82f57100274ffae6726bf3618d4d
Robert Rabe <robert.rabe1@icloud.com>
2016-10-13 09:52:52 +0200

    Add tableview cells for longer build info as well as test results

---
+++ JenkinsiOS/LongBuildInfoTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  LongBuildInfoTableViewCell.swift
+//  JenkinsiOS
+//
+//  Created by Robert on 11.10.16.
+//  Copyright © 2016 MobiLab Solutions. All rights reserved.
+//
+
+import UIKit
+
+class LongBuildInfoTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+}
