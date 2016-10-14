b643cb1220397b025e37039190381dd6cd945fa9
Robert Rabe <robert.rabe1@icloud.com>
2016-10-14 16:22:42 +0200

    Add ability to edit accounts

--- JenkinsiOS/AccountsTableViewController.swift
+++ JenkinsiOS/AccountsTableViewController.swift
@@ -18,18 +18,27 @@ class AccountsTableViewController: UITableViewController {
         super.viewDidLoad()
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 50
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.reloadData()
+        navigationItem.leftBarButtonItem = editButtonItem
+        registerForPreviewing(with: self, sourceView: tableView)
     }
     
     //MARK: - View controller navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let accountCell = sender as? UITableViewCell, let dest = segue.destination as? JobsTableViewController, segue.identifier == Constants.Identifiers.showJobsSegue, let indexPath = tableView.indexPath(for: accountCell){
-            dest.account = AccountManager.manager.accounts[indexPath.row]
+        if let indexPath = sender as? IndexPath, let dest = segue.destination as? JobsTableViewController, segue.identifier == Constants.Identifiers.showJobsSegue{
+            prepare(viewController: dest, indexPath: indexPath)
+        }
+        else if segue.identifier == Constants.Identifiers.editAccountSegue, let dest = segue.destination as? AddAccountTableViewController, let indexPath = sender as? IndexPath{
+            prepare(viewController: dest, indexPath: indexPath)
+        }
+    }
+    
+    fileprivate func prepare(viewController: UIViewController, indexPath: IndexPath){
+        if let addAccountViewController = viewController as? AddAccountTableViewController{
+            addAccountViewController.account = AccountManager.manager.accounts[indexPath.row]
+        }
+        else if let jobsViewController = viewController as? JobsTableViewController{
+            jobsViewController.account = AccountManager.manager.accounts[indexPath.row]
         }
     }
     
@@ -55,6 +64,19 @@ class AccountsTableViewController: UITableViewController {
         return section == 0 ? 1 : AccountManager.manager.accounts.count
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard indexPath.section == 1
+            else { return }
+        
+        if isEditing{
+            performSegue(withIdentifier: Constants.Identifiers.editAccountSegue, sender: indexPath)
+        }
+        else{
+            performSegue(withIdentifier: Constants.Identifiers.showJobsSegue, sender: indexPath)
+        }
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return headers.count
     }
@@ -72,20 +94,48 @@ class AccountsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
-        return indexPath.section == 1 ? .delete : .none
+        return .delete
     }
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        return [
+            UITableViewRowAction(style: .destructive, title: "Delete", handler: { (_, indexPath) in
+                do{
+                    try AccountManager.manager.deleteAccount(account: AccountManager.manager.accounts[indexPath.row])
+                    self.tableView.reloadData()
+                }
+                catch{
+                    self.displayError(title: "Error", message: "Something went wrong", textFieldConfigurations: [], actions: [
+                            UIAlertAction(title: "Alright", style: .cancel, handler: nil)
+                        ])
+                    self.tableView.reloadData()
+                }
+            }),
+            UITableViewRowAction(style: .normal, title: "Edit", handler: { (_, indexPath) in
+                self.performSegue(withIdentifier: Constants.Identifiers.editAccountSegue, sender: indexPath)
+            })
+        ]
+    }
+}
+
+extension AccountsTableViewController: UIViewControllerPreviewingDelegate{
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        navigationController?.pushViewController(viewControllerToCommit, animated: true)
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        // The selected table view cell should be selected while we are not editing and should be in the accounts section, instead of the favorites section
+        guard let indexPath = tableView.indexPathForRow(at: location), isEditing == false, indexPath.section == 1
+            else { return nil }
+        previewingContext.sourceRect = tableView.rectForRow(at: indexPath)
+        guard let jobsViewController = (UIApplication.shared.delegate as? AppDelegate)?.getViewController(name: "JobsTableViewController")
+            else { return nil }
+        prepare(viewController: jobsViewController, indexPath: indexPath)
         
-        if editingStyle == .delete{
-            do{
-                try AccountManager.manager.deleteAccount(account: AccountManager.manager.accounts[indexPath.row])
-                tableView.reloadData()
-            }
-            catch{
-                //FIXME: Actually display an error
-                print(error)
-            }
-        }
+        return jobsViewController
     }
 }
--- JenkinsiOS/AddAccountTableViewController.swift
+++ JenkinsiOS/AddAccountTableViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class AddAccountTableViewController: UITableViewController {
     
+    //MARK: - Instance variables
+    
+    var account: Account?
+    
     //MARK: - Outlets
     
     @IBOutlet weak var addAccountButton: UIButton!
@@ -27,8 +31,17 @@ class AddAccountTableViewController: UITableViewController {
     }
     
     func addAccount(){
-        guard let url = URL(string: "https://" + urlTextField.text!)
+        guard let account = createAccount()
             else { return }
+        
+        AccountManager.manager.addAccount(account: account)
+        ApplicationUserManager.manager.save()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func createAccount() -> Account?{
+        guard let url = URL(string: "https://" + urlTextField.text!)
+            else { return nil }
         
         let port = Int(portTextField.text!) ?? Constants.Defaults.defaultPort
         let username = usernameTextField.text != "" ? usernameTextField.text : nil
@@ -36,41 +49,60 @@ class AddAccountTableViewController: UITableViewController {
         
         let displayName = nameTextField.text != "" ? nameTextField.text : nil
         
-        let account = Account(baseUrl: url, username: username, password: password, port: port, displayName: displayName)
-        AccountManager.manager.addAccount(account: account)
+        return Account(baseUrl: url, username: username, password: password, port: port, displayName: displayName)
+    }
+    
+    func saveAccount(){
+        guard let newAccount = createAccount()
+            else { return }
+        account?.baseUrl = newAccount.baseUrl
+        account?.displayName = newAccount.displayName
+        account?.password = newAccount.password
+        account?.username = newAccount.username
+        account?.port = newAccount.port
         
+        AccountManager.manager.save()
         dismiss(animated: true, completion: nil)
     }
     
     //MARK: - View Controller lifecycle
     
     override func viewDidLoad() {
+
+        // Write all known data into the text fields
+        if let account = account{
+            addAccountButton.setTitle("Save", for: .normal)
+            addAccountButton.addTarget(self, action: #selector(saveAccount), for: .touchUpInside)
+            usernameTextField.text = account.username ?? ""
+            apiKeyTextField.text = account.password ?? ""
+            urlTextField.text = account.baseUrl.absoluteString.replacingOccurrences(of: account.baseUrl.scheme?.appending("://") ?? "", with: "")
+            nameTextField.text = account.displayName ?? ""
+        }
+        else{
+            addAccountButton.addTarget(self, action: #selector(addAccount), for: .touchUpInside)
+            usernameTextField.text = ""
+            apiKeyTextField.text = ""
+        }
+
         // The add button should not be enabled when there is no text in the mandatory textfields
-        addAccountButton.isEnabled = false
-        
-        addAccountButton.addTarget(self, action: #selector(addAccount), for: .touchUpInside)
-        
+        addAccountButton.isEnabled = addButtonShouldBeEnabled()
         // For every mandatory textfield, add an event handler
         urlTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControlEvents.allEditingEvents)
         // For username and password textfields, set the default value to nil
-        
-        usernameTextField.text = nil
-        apiKeyTextField.text = nil
     }
     
     //MARK: - Textfield methods
     
     @objc private func textFieldChanged(){
-        
+        addAccountButton.isEnabled = addButtonShouldBeEnabled()
+    }
+    
+    func addButtonShouldBeEnabled() -> Bool{
         //Attention: a textField's text property is *never* nil, unless set to nil by the programmer
-
-        // The urlTextField's text should be a valid URL0
-        guard urlTextField.text != nil && URL(string: urlTextField.text!) != nil
-            else { return }
-        // The port text field's text should either be empty or a valid integer
-        guard portTextField.text == "" || Int(portTextField.text!) != nil
-            else { return }
         
-        addAccountButton.isEnabled = true
+        // The urlTextField's text should be a valid URL
+        // The port text field's text should either be empty or a valid integer
+
+        return urlTextField.text != nil && URL(string: urlTextField.text!) != nil && (portTextField.text == "" || Int(portTextField.text!) != nil)
     }
 }
