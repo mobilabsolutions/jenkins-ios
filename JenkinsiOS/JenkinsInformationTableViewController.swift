7e70bc0c058a7fbf3191b7b7e455c5e6dc64b69d
Robert Rabe <robert.rabe1@icloud.com>
2016-10-21 14:49:53 +0200

    JENIOS-9 Refactor presenting success and error messages after performing an action

--- JenkinsiOS/JenkinsInformationTableViewController.swift
+++ JenkinsiOS/JenkinsInformationTableViewController.swift
@@ -15,25 +15,36 @@ class JenkinsInformationTableViewController: UITableViewController {
     
     func performAction(action: JenkinsAction){
         
+        // Methods for presenting messages to the user
+        func showSuccessMessage(){
+            self.displayError(title: "Success", message: "The action was completed", textFieldConfigurations: [], actions: [
+                UIAlertAction(title: "Alright", style: .cancel, handler: nil)
+                ])
+        }
+        
+        func showFailureMessage(error: Error){
+            self.displayNetworkError(error: error, onReturnWithTextFields: { (data) in
+                self.account?.password = data["password"]!
+                self.account?.username = data["username"]!
+                self.performAction(action: action)
+            })
+        }
+        
         guard let account = account
             else { return }
-        
+        // Perform request
         NetworkManager.manager.perform(action: action, on: account) { (error) in
             DispatchQueue.main.async {
                 if let error = error{
-                    
                     if let networkManagerError = error as? NetworkManagerError, case let .HTTPResponseNoSuccess(code, _) = networkManagerError, Constants.Networking.successCodes.contains(code) || code == 503{
-                        self.displayError(title: "Success", message: "The action was completed", textFieldConfigurations: [], actions: [
-                                UIAlertAction(title: "Alright", style: .cancel, handler: nil)
-                            ])
+                        showSuccessMessage()
                     }
                     else {
-                        self.displayNetworkError(error: error, onReturnWithTextFields: { (data) in
-                            self.account?.password = data["password"]!
-                            self.account?.username = data["username"]!
-                            self.performAction(action: action)
-                        })
+                        showFailureMessage(error: error)
                     }
+                }
+                else{
+                    showSuccessMessage()
                 }
             }
         }
@@ -41,7 +52,7 @@ class JenkinsInformationTableViewController: UITableViewController {
     
     // MARK: - Table view delegate and datasource
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let account = account, indexPath.section == 1
+        guard account != nil, indexPath.section == 1
             else { return }
         performAction(action: actions[indexPath.row])
         tableView.deselectRow(at: indexPath, animated: true)
