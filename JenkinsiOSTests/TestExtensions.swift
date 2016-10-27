9500840b7ba0ab990ceddf37d1c32910312b23d3
Robert Rabe <robert@mobilabsolutions.com>
2016-10-27 18:16:51 +0200

    #JENIOS-15 Add first tests for Builds.

--- JenkinsiOSTests/TestExtensions.swift
+++ JenkinsiOSTests/TestExtensions.swift
@@ -17,12 +17,4 @@ func AssertNotEmpty<T>(_ value: [T]){
     XCTAssertFalse(value.isEmpty)
 }
 
-func jsonForResource(name: String, extension fileExtension: String?, type: AnyClass) -> Any?{
-    guard let url = Bundle(for: type).url(forResource: name, withExtension: fileExtension)
-        else { XCTFail("Bundle URL could not be found"); return nil }
-    guard let data = try? Data(contentsOf: url)
-        else { XCTFail("Could not get data for resource file"); return nil }
-    guard let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
-        else { XCTFail("Could not get json in correct format"); return nil }
-    return json
-}
+
