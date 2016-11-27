2d90089d2e5503a113f194087d341d52a5372dc8
Robert Rabe <robert@mobilabsolutions.com>
2016-11-27 11:05:31 +0100

    JENIOS-16 #comment Add method to parse a Jenkins account from a plist for later tests.

---
+++ JenkinsiOSTests/JenkinsAccountReaderTests.swift
@@ -0,0 +1,23 @@
+//
+//  JenkinsAccountReaderTests.swift
+//  JenkinsiOS
+//
+//  Created by Robert on 27.11.16.
+//  Copyright © 2016 MobiLab Solutions. All rights reserved.
+//
+@testable import JenkinsiOS
+import XCTest
+
+class JenkinsAccountReaderTests: XCTestCase {
+    
+    func testParsesAccountCorrectly() {
+        let account = JenkinsAccountReader.getAccount(for: type(of: self))
+        
+        XCTAssertNotNil(account)
+        XCTAssertEqual(account?.baseUrl, URL(string: "YOUR_JENKINS_URL_HERE"))
+        XCTAssertEqual(account?.username, "YOUR JENKINS USERNAME HERE")
+        XCTAssertEqual(account?.password, "YOUR JENKINS API-KEY HERE")
+        XCTAssertEqual(account?.port, 443)
+    }
+    
+}
