4583bbcef9cc6c6c04f5014e2df27c15753c7caf
Robert Rabe <robert@mobilabsolutions.com>
2017-10-21 17:52:19 +0200

    JENIOS-46 Update tests

--- JenkinsiOSTests/TestExtensions.swift
+++ JenkinsiOSTests/TestExtensions.swift
@@ -24,10 +24,10 @@ func assertArraysAreEqual(array1: [Any?], array2: [Any?]){
     for (index, element) in array1.enumerated(){
         
         guard (element != nil && array2[index] != nil)
-            else { if(!(element == nil && array2[index] == nil)){ XCTFail("\(element) is not the same as \(array2[index])") }; continue }
+            else { if(!(element == nil && array2[index] == nil)){ XCTFail("\(String(describing: element)) is not the same as \(String(describing: array2[index]))") }; continue }
         
         if((element as? NSObject) != (array2[index] as? NSObject)){
-            XCTFail("\(element) is not the same as \(array2[index])")
+            XCTFail("\(String(describing: element)) is not the same as \(String(describing: array2[index]))")
         }
     }
 }
